12a3aea75f6b2e8c9aeda0f0c3b9bf2766bc1e74
sateayamsuper <banuseta234@gmail.com>
2025-03-19 00:50:54 +0900

    rust_exercise

---
+++ target/debug/deps/rust_exercise-c969a081817c7984.d
@@ -0,0 +1,5 @@
+/Users/banusetaanindita/Documents/GitHub/open-rust/rust_exercise/target/debug/deps/rust_exercise-c969a081817c7984: src/main.rs
+
+/Users/banusetaanindita/Documents/GitHub/open-rust/rust_exercise/target/debug/deps/rust_exercise-c969a081817c7984.d: src/main.rs
+
+src/main.rs:
